df1ef9d8ef74f5a46079b4b50f1f1c021549c124
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-03-19 15:30:16 +0100

    Add dark mode setting

--- frontend/lib/pages/settings_page/settings_page.dart
+++ frontend/lib/pages/settings_page/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/themes/theme_manager.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -6,7 +8,55 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: Theme.of(context)
+              .textTheme
+              .displayMedium!
+              .apply(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: const [
+          DarkModeSetting(),
+        ],
+      ),
+    );
+  }
+}
+
+class DarkModeSetting extends StatelessWidget {
+  const DarkModeSetting({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final themeManager = Provider.of<ThemeManager>(context);
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.only(top: 8, left: 2, right: 2),
+      color: Theme.of(context).colorScheme.onPrimary,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Dark mode',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Switch(
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              value: themeManager.themeMode == ThemeMode.dark,
+              onChanged: (value) {
+                themeManager.toggleTheme();
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
