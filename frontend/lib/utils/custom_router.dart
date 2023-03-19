fbb9916cebfb7444a4e62f4aa68e09f95f0fc42e
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-03-19 13:33:38 +0100

    Create custom router

---
+++ frontend/lib/utils/custom_router.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class RouterAnimation {
+  static SlideTransition Function(
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) get bottomToTop {
+    return (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    };
+  }
+
+  static SlideTransition Function(
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) get leftToRight {
+    return (context, animation, secondaryAnimation, child) {
+      const begin = Offset(-1.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    };
+  }
+
+  static SlideTransition Function(
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) get rightToLeft {
+    return (context, animation, secondaryAnimation, child) {
+      const begin = Offset(1.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    };
+  }
+}
+
+class CustomRouter {
+  static Route _createPageRoute(
+      Widget page,
+      SlideTransition Function(
+              BuildContext context,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+              Widget child)
+          animation) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: animation,
+    );
+  }
+
+  static void push({
+    required BuildContext context,
+    required Widget page,
+    required SlideTransition Function(
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child)
+        animation,
+  }) {
+    Navigator.of(context).push(_createPageRoute(page, animation));
+  }
+}
