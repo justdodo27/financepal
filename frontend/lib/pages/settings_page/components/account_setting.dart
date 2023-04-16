6a946d998ee74dd52c45d4b2ae7bd654bdc94bc2
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-04-16 22:11:09 +0200

    Add logging out

---
+++ frontend/lib/pages/settings_page/components/account_setting.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/components/rounded_outlined_button.dart';
+import 'package:frontend/pages/accounts/login_consumer.dart';
+import 'package:frontend/pages/accounts/login_page.dart';
+import 'package:frontend/providers/api_provider.dart';
+import 'package:frontend/utils/custom_router.dart';
+import 'package:provider/provider.dart';
+
+import '../../main_page/main_page.dart';
+
+class AccountSetting extends StatefulWidget {
+  const AccountSetting({
+    super.key,
+  });
+
+  @override
+  State<AccountSetting> createState() => _AccountSettingState();
+}
+
+class _AccountSettingState extends State<AccountSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.only(top: 8, left: 4, right: 4),
+      color: Theme.of(context).colorScheme.onPrimary,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Log out',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            SmallRoundedOutlinedTextButton(
+              padding: EdgeInsets.zero,
+              label: 'Log out',
+              onPressed: () async {
+                await Provider.of<BackendApi>(context, listen: false).logOut();
+                if (!mounted) return;
+                CustomRouter.pushReplacement(
+                  context: context,
+                  page: const LoginConsumer(),
+                  animation: RouterAnimation.leftToRight,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
