6b46e89b6c9a2f69888eebff64c7aa39bf80b412
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-04-17 14:36:47 +0200

    Connect categories with API

--- frontend/lib/pages/categories_page/components/add_category_sheet.dart
+++ frontend/lib/pages/categories_page/components/add_category_sheet.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/providers/api_provider.dart';
+import 'package:frontend/utils/api/category.dart';
+import 'package:frontend/utils/snackbars.dart';
+import 'package:provider/provider.dart';
 
 import '../../../components/custom_text_field.dart';
 import '../../../components/rounded_outlined_button.dart';
 
 class AddCategorySheet extends StatefulWidget {
+  final Category? category;
+
   const AddCategorySheet({
     super.key,
+    this.category,
   });
 
   @override
@@ -13,8 +20,40 @@ class AddCategorySheet extends StatefulWidget {
 }
 
 class _AddCategorySheetState extends State<AddCategorySheet> {
+  final _formKey = GlobalKey<FormState>();
   final _name = TextEditingController();
   final _group = TextEditingController();
+
+  void createCategory() async {
+    try {
+      await Provider.of<BackendApi>(context, listen: false)
+          .addCategory(Category(name: _name.text));
+    } on Exception catch (e) {
+      showExceptionSnackBar(context, e);
+    } finally {
+      Navigator.of(context).pop();
+    }
+  }
+
+  void editCategory() async {
+    try {
+      widget.category!.name = _name.text;
+      await Provider.of<BackendApi>(context, listen: false)
+          .editCategory(widget.category!);
+    } on Exception catch (e) {
+      showExceptionSnackBar(context, e);
+    } finally {
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.category != null) {
+      _name.text = widget.category!.name;
+    }
+  }
 
   @override
   void dispose() {
@@ -27,67 +66,73 @@ class _AddCategorySheetState extends State<AddCategorySheet> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Text(
-              'Add a category',
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 8),
-            Card(
-              elevation: 1,
-              color: Theme.of(context).colorScheme.secondary,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Text(
+                'Add a category',
+                style: Theme.of(context).textTheme.bodyLarge,
               ),
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 16,
+              const SizedBox(height: 8),
+              Card(
+                elevation: 1,
+                color: Theme.of(context).colorScheme.secondary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
                 ),
-                child: Column(
-                  children: [
-                    CustomTextField(
-                      controller: _name,
-                      hintText: 'e.g. Groceries',
-                      labelText: 'Name',
-                    ),
-                    CustomTextField(
-                      controller: _group,
-                      hintText: 'e.g. Friends',
-                      labelText: 'Group (optional)',
-                    ),
-                    const SizedBox(height: 16),
-                    RoundedOutlinedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12,
+                    horizontal: 16,
+                  ),
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        controller: _name,
+                        hintText: 'e.g. Groceries',
+                        labelText: 'Name',
+                        validator: (value) => value!.isEmpty
+                            ? 'Enter at least one character.'
+                            : null,
                       ),
-                      width: double.infinity,
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      backgroundColor: Theme.of(context).colorScheme.tertiary,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Text(
-                          'Submit',
-                          style: Theme.of(context).textTheme.bodyMedium,
+                      // CustomTextField(
+                      //   controller: _group,
+                      //   hintText: 'e.g. Friends',
+                      //   labelText: 'Group (optional)',
+                      // ),
+                      const SizedBox(height: 16),
+                      RoundedOutlinedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
                         ),
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    )
-                  ],
+                        width: double.infinity,
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        backgroundColor: Theme.of(context).colorScheme.tertiary,
+                        onPressed: widget.category == null
+                            ? createCategory
+                            : editCategory,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Text(
+                            'Submit',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                bottom: MediaQuery.of(context).viewInsets.bottom,
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- frontend/lib/pages/categories_page/components/category_tile.dart
+++ frontend/lib/pages/categories_page/components/category_tile.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/providers/api_provider.dart';
+import 'package:frontend/utils/snackbars.dart';
+import 'package:provider/provider.dart';
 
-import '../categories_page.dart';
+import '../../../utils/api/category.dart';
+import 'add_category_sheet.dart';
 
 class CategoryTile extends StatelessWidget {
   final Category category;
@@ -10,6 +14,17 @@ class CategoryTile extends StatelessWidget {
     required this.category,
   });
 
+  void deleteCategory(BuildContext context) async {
+    try {
+      await Provider.of<BackendApi>(context, listen: false)
+          .deleteCategory(category);
+    } on Exception catch (e) {
+      showExceptionSnackBar(context, e);
+    } finally {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,68 +33,112 @@ class CategoryTile extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
       ),
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Flexible(
-              child: Row(
-                children: [
-                  category.isGroupCategory
-                      ? const GroupCategoryIcon()
-                      : const Icon(Icons.category, color: Colors.blue),
-                  const SizedBox(width: 16),
-                  Flexible(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          category.name,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        if (category.isGroupCategory)
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(minHeight: 65),
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: Row(
+                  children: [
+                    category.isGroupCategory
+                        ? const GroupCategoryIcon()
+                        : const Icon(Icons.category, color: Colors.blue),
+                    const SizedBox(width: 16),
+                    Flexible(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
                           Text(
-                            category.group!,
+                            category.name,
                             overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodySmall!
-                                .apply(color: Colors.grey),
                           ),
-                      ],
+                          if (category.isGroupCategory)
+                            Text(
+                              'Group name',
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .apply(color: Colors.grey),
+                            ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.delete),
-                    iconSize: 22,
-                    color: Theme.of(context).textTheme.bodyMedium!.color,
-                    splashRadius: 22,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.edit),
-                    iconSize: 22,
-                    color: Theme.of(context).textTheme.bodyMedium!.color,
-                    splashRadius: 22,
-                  ),
-                ),
-              ],
-            )
-          ],
+              if (!category.isGeneralCategory)
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      child: IconButton(
+                        onPressed: () => showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.background,
+                            title: Text(
+                              'Are you sure you want to delete the "${category.name}" category?',
+                              style: Theme.of(context).textTheme.displayMedium,
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => deleteCategory(context),
+                                child: Text(
+                                  'Yes',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text(
+                                  'No',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        icon: const Icon(Icons.delete),
+                        iconSize: 22,
+                        color: Theme.of(context).textTheme.bodyMedium!.color,
+                        splashRadius: 22,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      child: IconButton(
+                        onPressed: () => showModalBottomSheet(
+                          context: context,
+                          backgroundColor:
+                              Theme.of(context).colorScheme.onPrimary,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(30),
+                              topLeft: Radius.circular(30),
+                            ),
+                          ),
+                          useSafeArea: true,
+                          isScrollControlled: true,
+                          builder: (context) =>
+                              AddCategorySheet(category: category),
+                        ),
+                        icon: const Icon(Icons.edit),
+                        iconSize: 22,
+                        color: Theme.of(context).textTheme.bodyMedium!.color,
+                        splashRadius: 22,
+                      ),
+                    ),
+                  ],
+                )
+            ],
+          ),
         ),
       ),
     );
