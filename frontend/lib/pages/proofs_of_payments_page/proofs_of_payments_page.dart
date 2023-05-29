6349badda1f7147ad83a04925e43478ff8c69f54
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-05-29 20:28:50 +0200

    Update proof of payments page and widgets

--- frontend/lib/pages/proofs_of_payments_page/proofs_of_payments_page.dart
+++ frontend/lib/pages/proofs_of_payments_page/proofs_of_payments_page.dart
@@ -1,81 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/proofs_of_payments_page/components/add_payment_proof_sheet.dart';
+import 'package:frontend/providers/payment_proof_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'components/proof_of_payment_tile.dart';
 
-class PaymentProof {
-  final String name;
-  final String url;
-
-  PaymentProof({required this.name, required this.url});
-}
-
-class ProofsOfPaymentsPage extends StatelessWidget {
+class ProofsOfPaymentsPage extends StatefulWidget {
   const ProofsOfPaymentsPage({super.key});
 
-  static List<PaymentProof> data = [
-    PaymentProof(
-        name: 'Receipt Biedronka 22/03/2023',
-        url:
-            'https://d-art.ppstatic.pl/kadry/k/r/1/3b/18/61712ef03f711_o_full.jpg'),
-    PaymentProof(
-        name: 'Receipt Auchan 21/03/2023',
-        url:
-            'https://twojepajeczno.pl/wp-content/uploads/2015/10/Paragon_czyt.png'),
-    PaymentProof(
-        name: 'Receipt Lidl 20/03/2023',
-        url:
-            'https://pomoc.ipos.pl/hc/article_attachments/4405692742290/FISCAL.TRANSACTION-NIP-nabywcy.txt__1_.png'),
-    PaymentProof(
-        name: 'Receipt Biedronka 19/03/2023',
-        url:
-            'https://d-art.ppstatic.pl/kadry/k/r/1/3b/18/61712ef03f711_o_full.jpg'),
-    PaymentProof(
-        name: 'Receipt Auchan 18/03/2023',
-        url:
-            'https://twojepajeczno.pl/wp-content/uploads/2015/10/Paragon_czyt.png'),
-    PaymentProof(
-        name: 'Receipt Lidl 17/03/2023',
-        url:
-            'https://pomoc.ipos.pl/hc/article_attachments/4405692742290/FISCAL.TRANSACTION-NIP-nabywcy.txt__1_.png'),
-    PaymentProof(
-        name: 'Receipt Biedronka 16/03/2023',
-        url:
-            'https://d-art.ppstatic.pl/kadry/k/r/1/3b/18/61712ef03f711_o_full.jpg'),
-    PaymentProof(
-        name: 'Receipt Auchan 15/03/2023',
-        url:
-            'https://twojepajeczno.pl/wp-content/uploads/2015/10/Paragon_czyt.png'),
-    PaymentProof(
-        name: 'Receipt Lidl 14/03/2023',
-        url:
-            'https://pomoc.ipos.pl/hc/article_attachments/4405692742290/FISCAL.TRANSACTION-NIP-nabywcy.txt__1_.png'),
-  ];
+  @override
+  State<ProofsOfPaymentsPage> createState() => _ProofsOfPaymentsPageState();
+}
 
-  List<Widget> _getRows() {
-    return data.map((proof) => PaymentProofTile(proof: proof)).toList();
-  }
-
+class _ProofsOfPaymentsPageState extends State<ProofsOfPaymentsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: ListView(
         physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              elevation: 4,
-              color: Theme.of(context).colorScheme.onPrimary,
-              child: Column(
-                children: _getRows(),
-              ),
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
             ),
-            const SizedBox(height: 80),
-          ],
-        ),
+            elevation: 4,
+            color: Theme.of(context).colorScheme.onPrimary,
+            child: Consumer<PaymentProofProvider>(
+              builder: (context, provider, child) {
+                final paymentProofs = provider.paymentProofs ?? [];
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: paymentProofs.length,
+                  itemBuilder: (context, index) =>
+                      PaymentProofTile(proof: paymentProofs[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Opacity(
         opacity: 0.85,
