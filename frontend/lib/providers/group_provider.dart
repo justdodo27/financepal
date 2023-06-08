77e36c0efb157f0798f56cbdc38996eea0f21b68
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-06-08 20:10:33 +0200

    Add new methods to group provider

--- frontend/lib/providers/group_provider.dart
+++ frontend/lib/providers/group_provider.dart
@@ -13,6 +13,8 @@ class GroupProvider extends ChangeNotifier {
 
   List<Group>? get groups => _groups;
 
+  bool get isLoading => _groups == null;
+
   /// Fetches list of the user's groups from the backend.
   Future<void> fetchGroups() async {
     if (auth == null) return;
@@ -24,5 +26,53 @@ class GroupProvider extends ChangeNotifier {
     } catch (_) {
       throw Exception('Failed to load the groups.');
     }
+    notifyListeners();
+  }
+
+  /// Creates a new group.
+  Future<void> createGroup(String name) async {
+    if (auth == null) return;
+    final token = auth!.token;
+    if (token == null) return;
+
+    try {
+      final created =
+          await auth!.apiService.createGroup(token, groupName: name);
+      _groups!.add(created);
+    } catch (_) {
+      throw Exception('Failed to create the group.');
+    }
+    notifyListeners();
+  }
+
+  /// Joins a group.
+  Future<void> joinGroup(String code) async {
+    if (auth == null) return;
+    final token = auth!.token;
+    if (token == null) return;
+
+    try {
+      await auth!.apiService.joinGroup(token, groupCode: code);
+    } catch (_) {
+      throw Exception('Failed to join the group.');
+    }
+
+    await fetchGroups();
+  }
+
+  /// Deletes a group.
+  Future<void> deleteGroup(int groupId) async {
+    if (auth == null) return;
+    final token = auth!.token;
+    if (token == null) return;
+
+    try {
+      await auth!.apiService.deleteGroup(token, groupId: groupId);
+      _groups!.removeWhere((group) => group.id == groupId);
+    } catch (_) {
+      rethrow;
+    }
+
+    notifyListeners();
   }
 }
